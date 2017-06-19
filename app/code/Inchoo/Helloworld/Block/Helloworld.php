00e4a7f0a6cea4483cd2891a026623cd8b96f3ca
Saed Yousef <s.alzabaen@classera.com>
2017-06-19 11:49:41 +0300

    magento hello world module

---
+++ app/code/Inchoo/Helloworld/Block/Helloworld.php
@@ -0,0 +1,10 @@
+<?php
+namespace Inchoo\Helloworld\Block;
+ 
+class Helloworld extends \Magento\Framework\View\Element\Template
+{
+    public function getHelloWorldTxt()
+    {
+        return 'Hello world!';
+    }
+}
